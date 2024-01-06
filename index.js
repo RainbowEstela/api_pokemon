626a74cf827382fe55f0b4d6484a14d638c1f8ae
Estela <eloiseruiz15@gmail.com>
2024-01-06 10:14:55 +0100

    docker compose mas url mongo para contenedor

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const app = require("./app");
 const port = 3000;
-const urlMongoDb = "mongodb://localhost:27017/pokemondb?authSource=admin";
+//const urlMongoDb = "mongodb://localhost:27017/pokemondb?authSource=admin";
+const urlMongoDb = "mongodb://root:toor@mongodb:27017/pokemondb?authSource=admin";
 
 mongoose.connect(urlMongoDb, );
 
